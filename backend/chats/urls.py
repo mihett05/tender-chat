85288700f9322f835a88d7cd535c4cce2991c82b
AlexandrVino <alexander-vin0@yandex.ru>
2023-12-10 16:14:04 +0500

    update

--- backend/chats/urls.py
+++ backend/chats/urls.py
@@ -11,6 +11,6 @@ router.register('commits', views.CommitView, basename='commits')
 urlpatterns = [
     # path('contracts/', views.ContractCreateView.as_view(), name='contract_create'),
     # path('messages/', views.MessageCreateView.as_view({'post': 'create'}), name='message_create'),
-    # path('commits/', views.CommitCreateView.as_view({'post': 'create'}), name='commit_create'),
+    path('create_contract_docs/', views.CreateContractView.as_view(), name='create_contract_docs'),
     path('', include(router.urls)),
 ]
